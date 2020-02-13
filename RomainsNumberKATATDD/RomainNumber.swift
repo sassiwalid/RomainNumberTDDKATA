f7ba65225e74ec09814a52f34ae9b1623cd8ee4b
sassiwalid <sassi.walid@outlook.com>
2020-02-13 10:26:28 +0100

    First commit

--- RomainsNumberKATATDD/RomainNumber.swift
+++ RomainsNumberKATATDD/RomainNumber.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+
+class RomainNumber{
+    
+    func convertToArabic(_ romainNumber: String) -> Int {
+        switch romainNumber {
+        case "I":
+            return 1
+        case "V":
+            return 5
+        case "X":
+            return 10
+        case "L":
+            return 50
+        case "C":
+            return 100
+        case "D":
+            return 500
+        case "M":
+            return 1000
+        default:
+            return 0
+        }
+    }
+    
+}
--- RomainsNumberKATATDDTests/RomainsNumberKATATDDTests.swift
+++ RomainsNumberKATATDDTests/RomainsNumberKATATDDTests.swift
@@ -10,6 +10,7 @@ import XCTest
 @testable import RomainsNumberKATATDD
 
 class RomainsNumberKATATDDTests: XCTestCase {
+    let sut = RomainNumber()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,16 +20,61 @@ class RomainsNumberKATATDDTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testOneRomain_Should_Return_One_In_Arabic(){
+        let result = sut.convertToArabic("I")
+        
+        //then
+        XCTAssertEqual(result, 1, "One romain is not equal to 1")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testfiveRomain_Should_Return_five_In_Arabic(){
+        
+        let result = sut.convertToArabic("V")
+        
+        //then
+        XCTAssertEqual(result, 5, "One romain is not equal to 5")
     }
+    func testtenRomain_Should_Return_ten_In_Arabic(){
+        
+        let result = sut.convertToArabic("X")
+        
+        //then
+        XCTAssertEqual(result, 10, "One romain is not equal to 10")
+    }
+    func testFiftyRomain_Should_Return_Fifty_In_Arabic(){
+        
+        let result = sut.convertToArabic("L")
+        
+        //then
+        XCTAssertEqual(result, 50, "One romain is not equal to 50")
+    }
+    func testHundredRomain_Should_Return_hundred_In_Arabic(){
+        
+        let result = sut.convertToArabic("C")
+        
+        //then
+        XCTAssertEqual(result, 100, "One romain is not equal to 100")
+    }
+    func testFiveHundredRomain_Should_Return_Fivehundred_In_Arabic(){
+        
+        let result = sut.convertToArabic("D")
+        
+        //then
+        XCTAssertEqual(result, 500, "One romain is not equal to 500")
+    }
+    func testThousandRomain_Should_Return_Thousand_In_Arabic(){
+        
+        let result = sut.convertToArabic("M")
+        
+        //then
+        XCTAssertEqual(result, 1000, "One romain is not equal to 1000")
+    }
+    
+    func testEmptyRomainNumberShould_Return_Zero_In_Arabic(){
+        let result = sut.convertToArabic("")
+        //then
+        XCTAssertEqual(result, 0, "Empty Romain should return ZERO")
+    }
+    
+    
 
 }
